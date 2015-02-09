0b18df7a81385f63a46a0eb2066a9ecff61b16ae
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2015-02-08 19:25:24 -0500

    Included a simple demo that finds the typical image in the 2D HeLa microtubule dataset from the Murphy Lab at Carnegie Mellon University

--- octave/demos/typic_demo.m
+++ octave/demos/typic_demo.m
@@ -1,13 +1,44 @@
+function results = typic_demo()
+% TYPIC_DEMO Typic demo that downloads
+
+% Copyright (C) 2015 Ivan E. Cao-Berg
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published
+% by the Free Software Foundation; either version 2 of the License,
+% or (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+% General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+% 02110-1301, USA.
+%
+% For additional information visit http://github.com/icaoberg or
+% https://bitbucket.org/icaoberg
+
+filename = which( 'typic_demo' );
+[path,filename,extension] = fileparts( filename );
+cd( path );
+
 disp( 'Retrieving 2D Hela dataset from the Murphy Lab website' );
 get_2D_hela_images();
 
-disp( 'Retrieving subcellular location features for the 2D Hela collection' );
-data = get_2D_hela_features();
+disp( 'Loading subcellular location features for the 2D Hela collection' );
+load( ['..' filesep 'data' filesep ...
+    'HeLa10Class2DFeatures19990526.mat'] );
 
 indices = find(ismember(data.labels,'tubul'));
 features = data.features(size(data.features,1)-length(indices)+1:end,:);
 
-[sorted, indices, distances, numpc, pcntexplained] = typic( features );
+tic
+[sorted, indices, distances, numpc, pcntexplained ] = typic( features );
+disp( 'TyPic runnig time' );
+toc
 
 directory = [ pwd filesep 'data' filesep 'images' filesep 'Microtubules' ];
 files = dir( [ directory filesep '*.png' ] );
@@ -21,3 +52,12 @@ title( [ 'The most typical image in the 2D ' ...
     'Hela microtubule dataset is ' files(index).name ] );
 set(gcf,'units','normalized','outerposition',[0 0 1 1]);
 saveas(gcf,[pwd filesep 'typic_demo.png']);
+
+delete( '*.tgz' );
+rmdir( [ pwd filesep 'data' ], 's' );
+
+results.features = features;
+results.typic.sorted = sorted;
+results.typic.indices = indices;
+results.typic.distances = indices;
+results.typic.number_of_principal_components = numpc;
