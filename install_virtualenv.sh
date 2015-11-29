4d9182f69a76f4905be282707093e7d1a0c96a6a
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2015-11-29 04:17:15 -0500

    Updated installation script

--- install_virtualenv.sh
+++ install_virtualenv.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-virtualenv --system_site_packages .
+virtualenv --system-site-packages .
 source ./bin/activate
 
 pip install jupyter
 pip install -U ipython[all]
 pip install numpy
 pip install scipy
+pip install tabulate
+pip install pandas
+pip install scikit-learn
 
 deactivate
